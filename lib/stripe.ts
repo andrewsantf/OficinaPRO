8c0a0d0d3d65f5d02b14f3f872e9b1991eecddba
Andrew Gabriel <andrewsantf@gmail.com>
2025-12-10 10:22:56 -0300

    fix: atualizar versão da API do Stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2024-11-20.acacia', // Use latest API version or pin one
+    apiVersion: '2025-11-17.clover',
     typescript: true,
 });
